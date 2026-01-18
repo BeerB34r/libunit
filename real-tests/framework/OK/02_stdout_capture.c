7da722852b94db31c31cc881eb8b9a76640412b1
Mats de Beer <mde-beer@student.codam.nl>
2026-01-18 14:03:55 +0100

    OK tests

---
+++ real-tests/framework/OK/02_stdout_capture.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                            ::::::::        */
+/*   02_stdout_capture.c                                     :+:    :+:       */
+/*                                                          +:+               */
+/*   By: mde-beer <mde-beer@student.codam.nl>              +#+                */
+/*                                                        +#+                 */
+/*   Created: 2026/01/18 13:44:37 by mde-beer            #+#    #+#           */
+/*   Updated: 2026/01/18 13:49:53 by mde-beer            ########   odam.nl   */
+/*                                                                            */
+/*   —————No norm compliance?——————                                           */
+/*   ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝                                           */
+/*   ⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇                                           */
+/*   ⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀                                           */
+/*   ⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀⠀                                           */
+/*   ⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀                                           */
+/*   ⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀                                           */
+/*   ⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀                                           */
+/*   ⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀                                           */
+/*   ⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                           */
+/*   ⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                           */
+/*   ⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                           */
+/*   ⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                           */
+/*   ⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                           */
+/*   ——————————————————————————————                                           */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <framework.h>
+
+int	printing_dummy(void)
+{
+	write(STDOUT_FILENO, "Hello, World!\n", 14);
+	return (0);
+}
+
+int	stdout_capture_dummy(void)
+{
+	return (printing_test("Hello, World!\n", "", &printing_dummy));
+}
+
+int	stdout_capture_test(void)
+{
+	t_unit_ctx	*tests;
+
+	tests = create_ctx("dummy");
+	load_test(&tests, (t_test){
+		.name = "Stdout capture",
+		.func = &stdout_capture_dummy
+	});
+	load_test(&tests, (t_test){
+		.name = "Stdout capture",
+		.func = &stdout_capture_dummy
+	});
+	load_test(&tests, (t_test){
+		.name = "Stdout capture",
+		.func = &stdout_capture_dummy
+	});
+	return (launch_tests(&tests));
+}
